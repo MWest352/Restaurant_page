a944c34a5a0202696830784fbd99fd633c8b206b
MWest352 <MWest35285@gmail.com>
2022-12-05 12:48:50 -0700

    updates to AboutBody and AboutAppendSection

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -3,7 +3,6 @@
 function aboutSection() {
   const aboutSection = document.createElement('div');
 
-  aboutSection.innerHTML = "About Section"
   aboutSection.setAttribute('id', 'aboutSection');
 
   return aboutSection;
@@ -21,7 +20,7 @@ function aboutBody() {
   const aboutBody = document.createElement('div');
 
   aboutBody.textContent = 
-  "Johnny's Sliders was founded in 2022. Modled after an early diner, it was meant to have that classic burger joint drive-in feel."
+  "Johnny's Sliders was founded in 2022. Modeled after an early diner, it was meant to have that classic burger joint drive-in feel. A simple menu keeps things quick and easy. Don't forget to check out the Sunday Car Shows!"
   aboutBody.setAttribute('id', 'aboutBody');
 
   return aboutBody;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import './style.css';
+//IMPORT HEADER
 import { header } from './headerModule';
 import { title } from './headerModule';
 import {subtitle} from './headerModule';
+//IMPORT NAVBAR
 import { navBar } from './navBarModule';
 import { navItemAbout } from './navBarModule';
 import { navItemContact } from './navBarModule';
 import { navItemMenu } from './navBarModule';
+//IMPORT ABOUT
+import { aboutSection } from './aboutModule';
+import { aboutHeader } from './aboutModule';
+import { aboutBody } from './aboutModule';
 
 
+//MAIN CONTAINER
 function mainContainer() {
   const mainContainer = document.createElement('div');
 
@@ -28,3 +35,9 @@ document.getElementById('mainContainer').append(navBar());
 document.getElementById('navBar').append(navItemMenu());
 document.getElementById('navBar').append(navItemContact());
 document.getElementById('navBar').append(navItemAbout());
+
+
+//APPEND ABOUT SECTION
+document.getElementById('mainContainer').append(aboutSection());
+document.getElementById('aboutSection').append(aboutHeader());
+document.getElementById('aboutSection').append(aboutBody());
