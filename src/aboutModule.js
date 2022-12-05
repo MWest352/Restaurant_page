86c6c242044458e7e9e506033ab2571a6b12e949
MWest352 <MWest35285@gmail.com>
2022-12-05 12:32:28 -0700

    update About Module with initial contents

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -1,2 +1,34 @@
 //About Module
 
+function aboutSection() {
+  const aboutSection = document.createElement('div');
+
+  aboutSection.innerHTML = "About Section"
+  aboutSection.setAttribute('id', 'aboutSection');
+
+  return aboutSection;
+}
+function aboutHeader() {
+  const aboutHeader = document.createElement('div');
+
+  aboutHeader.innerHTML = "About"
+  aboutHeader.setAttribute('id', 'aboutHeader');
+
+  return aboutHeader;
+}
+
+function aboutBody() {
+  const aboutBody = document.createElement('div');
+
+  aboutBody.textContent = 
+  "Johnny's Sliders was founded in 2022. Modled after an early diner, it was meant to have that classic burger joint drive-in feel."
+  aboutBody.setAttribute('id', 'aboutBody');
+
+  return aboutBody;
+}
+
+export{
+  aboutSection,
+  aboutHeader,
+  aboutBody
+}
