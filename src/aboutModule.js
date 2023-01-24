5d70d66253dd2016e75c994a1aff387814a43a99
MWest352 <MWest35285@gmail.com>
2023-01-24 12:19:34 -0700

    update fonts and backgrounds

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -20,7 +20,7 @@ function aboutBody() {
   const aboutBody = document.createElement('div');
 
   aboutBody.textContent = 
-  "Johnny's Sliders was founded in 2022. Modeled after an early diner, it was meant to have that classic burger joint drive-in feel. A simple menu keeps things quick and easy. Don't forget to check out the Sunday Car Shows!"
+  "B. Goode's was founded in 2022. Modeled after an early diner, it was meant to have that classic burger joint drive-in feel. A simple menu keeps things quick and easy. Don't forget to check out the Sunday Car Shows!"
   aboutBody.setAttribute('id', 'aboutBody');
 
   return aboutBody;
